81556f57013778fa32bb8bb11a2af73d91a5d62f
miquelpomar <miguelpomar@alumnat.iesdamiahuguet.net>
2024-01-19 08:08:06 +0100

    avis si no té sis lletres

---
+++ Paraules.js
@@ -0,0 +1,13 @@
+const  paraula="camisa"
+function enviar(){
+    let paraula = document.getElementById('Analitza').value;
+    console.log(paraula.length);
+    if(paraula.length==6){
+        document.getElementById('Analitza')
+    }else{
+        window.alert('aquesta parula no té sis lletres')
+
+
+    }
+}
+
