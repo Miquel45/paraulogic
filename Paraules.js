baf8467a3a0c7150634513bd8af6b7e4e49e2270
miquelpomar <miguelpomar@alumnat.iesdamiahuguet.net>
2024-01-30 12:56:19 +0100

    Pinta de verd si es corecta

--- Paraules.js
+++ Paraules.js
@@ -1,13 +1,31 @@
-const  paraula="camisa"
+const  solucio="CAMISA"
 function enviar(){
-    let paraula = document.getElementById('Analitza').value;
+    let paraula = document.getElementById('Analitza').value.toUpperCase();
     console.log(paraula.length);
     if(paraula.length==6){
-        document.getElementById('Analitza')
+        console.log(paraula.length);
+        let lletres =''
+        for (let i =0;i<paraula.length;i++){
+
+            if(paraula.charAt(i)==solucio.charAt(i) ){
+                lletres+='<div class="slot green">'
+            } else if (paraula.charAt(i)==) {
+                lletres+='<div class="slot yellow">'
+            }
+            else {
+                lletres+='<div class="slot">'
+            }
+            if(paraula.charAt(i))
+
+
+            lletres+= paraula.charAt(i)
+            lletres+='</div>'
+
+        }
+        document.getElementById('solucions').innerHTML=lletres
     }else{
         window.alert('aquesta parula no té sis lletres')
-
-
     }
 }
+
 
