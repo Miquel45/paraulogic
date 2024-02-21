89143a0dc11e869bb3cd115d39c0da48ffab94c2
miquelpomar <miguelpomar@alumnat.iesdamiahuguet.net>
2024-02-21 13:36:19 +0100

    Actualització

--- Paraules.js
+++ Paraules.js
@@ -6,6 +6,7 @@ function enviar(){
     if(paraula.length==6){
         console.log(paraula.length);
         let lletres =''
+        lletres+="<div class=\"resposta\">"
         for (let i =0;i<paraula.length;i++){
             //Si la paraula és la solució la pinta de verd
             if(paraula.charAt(i)==solucio.charAt(i) ){
@@ -20,7 +21,7 @@ function enviar(){
             lletres+= paraula.charAt(i)
             lletres+='</div>'
         }
-        document.getElementById('solucions').innerHTML=lletres
+        document.getElementById('solucions').innerHTML+=lletres
     }else{
         //Si no te sis lletres t'avisa
         window.alert('aquesta parula no té sis lletres')
